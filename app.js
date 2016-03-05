76126c76d25d7005bf36df6140b7a4eb9220a5e8
Alston Lin <alstonlin812@gmail.com>
2016-03-05 09:17:53 -0500

    Uploading works

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-var app = require('express')();
-var multipart = require('multipart');
-var sys = require('sys');
-
-app.get('/', function (req, res) {
-    res.render(
-      '<form action="/upload" method="post" enctype="multipart/form-data">'+
-      '<input type="file" name="upload-file">'+
-      '<input type="submit" value="Upload">'+
-      '</form>'
-    );
+var express =   require("express");
+var multer  =   require('multer');
+var app         =   express();
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    filename =  file.fieldname + Date.now();
+    console.log("Uploaded " + filename);
+    callback(null, filename);
   }
-);
+});
 
-app.post('/upload', function (req, res) {
-    req.setBodyEncoding('binary');
+var upload = multer({ storage : storage}).single('content');
 
-    var stream = new multipart.Stream(req);
-    stream.addListener('part', function(part) {
-      part.addListener('body', function(chunk) {
-        var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(2);
-        var mb = (stream.bytesTotal / 1024 / 1024).toFixed(1);
-        sys.print("Uploading " + mb + "mb ("+progress+"%)\015");
-        // chunk could be appended to a file if the uploaded file needs to be saved
-      });
-    });
-    stream.addListener('complete', function() {
-      res.sendHeader(200, {'Content-Type': 'text/plain'});
-      res.sendBody('Thanks for playing!');
-      res.finish();
-      sys.puts("\n=> Done");
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload', function(req, res){
+    console.log("Request Received");
+    upload(req, res, function(err) {
+        if(err) {
+            console.log("ERROR: " + err);
+            return res.end("Error uploading file.");
+        } else{
+            res.end("Uploaded");
+        }
     });
 });
 
 app.listen(8080, function(){
-  console.log("Listening on port 8080!")
+    console.log("Working on port 8080");
 });
